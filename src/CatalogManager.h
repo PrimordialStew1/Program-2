fd6c398357e10264582cbc468d4689bc21dec1f1
Aaron Davis <aaron.m.davis@comcast.net>
2025-02-06 12:16:29 -0600

    First commit

---
+++ src/CatalogManager.h
@@ -0,0 +1,10 @@
+// CatalogManager.h - CatalogManager declaration
+// For program 2 assignment
+//
+// Author: Aaron Davis, this is entirely my own work.
+// I understand that I may be asked questions about my submission.
+// email: amd0047@uah.edu
+// for CS221, Spring 2025 Mrs. Delugach
+// Compiler g++
+
+#pragma once
